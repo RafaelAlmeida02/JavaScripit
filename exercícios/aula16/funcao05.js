904a10c4af9e232051ef485b584315468e894304
Rafael Silva <silvaalmeidarafael1@gmail.com>
2026-08-12 14:10:54 -0300

    curso finalizado

---
+++ exercícios/aula16/funcao05.js
@@ -0,0 +1,20 @@
+//RECURSIVIDADE
+function fatorial(n) {
+    if (n == 1){
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
+
+/*
+
+5! = 5 x 4 x3 x2 x1 = 120
+5! = 5 x 4! = 120
+
+n! = n x (n-1)!
+1! = 1
+
+*/
